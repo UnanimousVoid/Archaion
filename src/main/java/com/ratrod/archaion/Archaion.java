f2f2d0209ebc05f600c9a2be40cccd9e3b7d16e7
UnanimousVoid <unanimousentity24@gmail.com>
2026-07-22 18:06:26 +0300

    Initial Commit

---
+++ src/main/java/com/ratrod/archaion/Archaion.java
@@ -0,0 +1,31 @@
+package com.ratrod.archaion;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.resources.Identifier;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import org.slf4j.Logger;
+
+import java.util.Locale;
+
+@Mod(Archaion.MODID)
+public class Archaion {
+    public static final String MODID = "archaion";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public Archaion(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+
+    }
+
+    public static Identifier prefix(String path) {
+        return Identifier.fromNamespaceAndPath(MODID, path.toLowerCase(Locale.ROOT));
+    }
+}
